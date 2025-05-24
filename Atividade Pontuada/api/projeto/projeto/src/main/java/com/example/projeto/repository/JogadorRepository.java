cfc84dc0f3817a28c7d28167d9efc77a123cf01d
Lucas Sena <159453926+01Lucasena@users.noreply.github.com>
2025-05-24 20:29:15 -0300

    Add files via upload

---
+++ Atividade Pontuada/api/projeto/projeto/src/main/java/com/example/projeto/repository/JogadorRepository.java
@@ -0,0 +1,13 @@
+package com.example.projeto.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.projeto.model.Jogador;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface JogadorRepository extends JpaRepository<Jogador, Long> {
+
+
+}
